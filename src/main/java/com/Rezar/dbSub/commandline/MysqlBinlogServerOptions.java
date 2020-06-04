c3a0b266a08c3c51e92def5e7e7f0564dc3594ca
Rezar <yuwenhao@zmeng123.com>
2020-06-04 19:44:31 +0800

    调整options说明

--- src/main/java/com/Rezar/dbSub/commandline/MysqlBinlogServerOptions.java
+++ src/main/java/com/Rezar/dbSub/commandline/MysqlBinlogServerOptions.java
@@ -27,7 +27,7 @@ public class MysqlBinlogServerOptions {
 	private String clusterConfigFile;
 	@Parameter(names = { "-workFolder", "-wf" }, description = "服务存储文件的目录(默认为当前目录--[.]代表当前目录)", required = false)
 	private String workFloder = ".";
-	@Parameter(names = { "-maxStore", "-ms" }, description = "存储的单表的记录数上限,超过该数会清理掉历史一半的数据", required = false)
+	@Parameter(names = { "-maxStore", "-ms" }, description = "存储的单表的记录数上限,超过该数会清理超出的历史数据", required = false)
 	private String maxStoreEventCount = "100000";
 
 }
